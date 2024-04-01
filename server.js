11217ec56536fd5b9300d5dd3ae30f3fba5fb794
Cody Bergman <bergcm03@pfw.edu>
2024-04-01 16:43:22 -0400

    commit

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: 'Gas!Lighter2',
+    password: 'jxf9dvlt',
     database: 'mydb'
 });
 // Import necessary modules and setup your server
